9bfe0340337cd81a64adf6d61ecb2384e7846209
yeyun0423 <168630116+yeyun0423@users.noreply.github.com>
2024-06-02 00:34:32 +0900

    frist

---
+++ src/final_project/Main.java
@@ -0,0 +1,9 @@
+package final_project;
+
+public class Main {
+
+	public static void main(String[] args) {
+	 System.out.println("안녕");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module final_project {
+}
